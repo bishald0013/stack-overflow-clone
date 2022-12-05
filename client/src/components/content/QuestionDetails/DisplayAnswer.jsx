94b68d725bcce4411a5bf79f9ee3fafc63dda940
Bishal Deb <bishal986473@gmail.com>
2022-12-05 10:52:34 +0530

    display ans

--- client/src/components/content/QuestionDetails/DisplayAnswer.jsx
+++ client/src/components/content/QuestionDetails/DisplayAnswer.jsx
@@ -1,14 +1,13 @@
 import {useParams} from "react-router-dom" 
 
-
-
 function DisplayAnswer({a}) {
 
   return (
-    <div>
+    <div className="container">
       <h1 className="fs-5">{a.answerBody}</h1>
       <h1 className="fs-5">{a.userAnswered}</h1>
       <h1 className="fs-5">{a.asnweredOn}</h1>
+      <hr></hr>
     </div>
   )
 }
